31e556c2c4d0b27459b98f574b3c519e0c9c98df
EugeneFoxWox <98107337+EugeneFoxWox@users.noreply.github.com>
2022-01-26 20:17:00 +0300

    laba

--- laba3.py
+++ laba3.py
@@ -1,15 +1,33 @@
-#бери Х как переменную ввода
+def inputs():
+   while True:
+       x = int(input('Введите натур. число '))
+       if x > 0:
+           break
+       print('Неверное число')
+   return x
+  
 
 def x2(x):
   x/= 2
   return collatz(x)
 
+
 def x3_1(x):
   x = x*3 + 1
   return collatz(x)
   
-#коллатц мой
-def collatz(x):
 
+def collatz(x):
+  result = [x]
+    if x == 1:
+       pass
+    elif x%2 == 0:
+         result.extend(x2(x))
+    else:
+        result.extend(x3_1(x))
+    return result
   
-  if __name__ == '__main__':
+
+if __name__ == '__main__':
+  N = inputs()
+    print(collatz(N))
